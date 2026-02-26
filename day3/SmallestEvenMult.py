db03dff33f0eac80c426a136e208836620a9621b
rubebanourin <nourinrubeba@gmail.com>
2026-02-26 14:59:15 +0530

    Delete day3/SmallestEvenMult.py

--- day3/SmallestEvenMult.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def smallestEvenMultiple(self, n):
-        c=n
-        while(True):
-            if (n%2 == 0):
-             return n
-            else:
-             n+=c
