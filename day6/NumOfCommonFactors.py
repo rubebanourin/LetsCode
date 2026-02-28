a07c1539be52cd9545b976b42c6e8d06b30cc4f5
rubebanourin <nourinrubeba@gmail.com>
2026-02-28 11:39:25 +0530

    Number of Common Factors

---
+++ day6/NumOfCommonFactors.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        count = 0
+        for i in range(1,min(a,b)+1):
+            if (a%i==0) & (b%i==0):
+                count+=1
+        return count
