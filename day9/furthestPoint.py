ae672a3cb62139d035bfe2cacddfaa03a9e3bdd0
rubebanourin <nourinrubeba@gmail.com>
2026-04-03 00:33:45 +0530

    Delete day9/furthestPoint.py

--- day9/furthestPoint.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def furthestDistanceFromOrigin(self, moves):
-        l=moves.count('L')
-        r=moves.count('R')
-        b=moves.count('_')
-        if l>r:
-            return l-r+b
-        else:
-            return r-l+b
